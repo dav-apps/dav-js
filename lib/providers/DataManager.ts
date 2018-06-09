9c10f707bdc12631e38c5a8258f65ac0db35a046
Dav <dav2070@web.de>
2018-06-09 20:09:15 +0200

    Add bowser package and use localstorage as driver for localforage when using Firefox

--- lib/providers/DataManager.ts
+++ lib/providers/DataManager.ts
@@ -1,10 +1,20 @@
-var localforage = require('localforage');
+import * as localforage from "localforage";
 import * as Dav from '../Dav';
+var bowser = require('bowser');
+
+function Init(){
+   if(bowser.firefox){
+      // Use localstorage as driver
+      localforage.setDriver(localforage.LOCALSTORAGE);
+   }
+}
 
 export function SetUser(user: object){
+   Init();
    localforage.setItem(Dav.userKey, user);
 }
 
 export async function GetUser(){
+   Init();
    return await localforage.getItem(Dav.userKey);
 }
