a73735eb01c073f1a54af75ac8182627a6af7660
Dav <dav2070@web.de>
2019-01-17 14:18:18 +0100

    Add Constants file for test constants

---
+++ test/Constants.ts
@@ -0,0 +1,7 @@
+export const davClassLibraryTestUserXTestUserJwt = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRhdmNsYXNzbGlicmFyeXRlc3RAZGF2LWFwcHMudGVjaCIsInVzZXJuYW1lIjoiZGF2Q2xhc3NMaWJyYXJ5VGVzdFVzZXIiLCJ1c2VyX2lkIjoxMiwiZGV2X2lkIjoyLCJleHAiOjM3NTI5MTgzODQxfQ.lO-iq5UHk25wnysbrEw1PirgGBhz-rFqrK6iRGkcFnU";
+export const davClassLibraryTestAppId = 12;
+export const davClassLibraryTestUserId = 12;
+export const testDataTableId = 23;
+
+export const firstPropertyName = "page1";
+export const secondPropertyName = "page2";
--- test/providers/DataManager.spec.ts
+++ test/providers/DataManager.spec.ts
@@ -6,27 +6,20 @@ import * as DatabaseOperations from '../../lib/providers/DatabaseOperations';
 import * as DataManager from '../../lib/providers/DataManager';
 import * as localforage from "localforage";
 import { TableObject, TableObjectUploadStatus, ConvertIntToVisibility, ConvertObjectToMap } from '../../lib/models/TableObject';
-
-const davClassLibraryTestUserXTestUserJwt = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRhdmNsYXNzbGlicmFyeXRlc3RAZGF2LWFwcHMudGVjaCIsInVzZXJuYW1lIjoiZGF2Q2xhc3NMaWJyYXJ5VGVzdFVzZXIiLCJ1c2VyX2lkIjoxMiwiZGV2X2lkIjoyLCJleHAiOjM3NTI5MTgzODQxfQ.lO-iq5UHk25wnysbrEw1PirgGBhz-rFqrK6iRGkcFnU";
-const davClassLibraryTestAppId = 12;
-const davClassLibraryTestUserId = 12;
-const testDataTableId = 23;
-
-const firstPropertyName = "page1";
-const secondPropertyName = "page2";
+import * as Constants from '../Constants';
 
 var firstTestDataTableObject = new TableObject();
 firstTestDataTableObject.Uuid = "642e6407-f357-4e03-b9c2-82f754931161";
 firstTestDataTableObject.Properties = new Map([
-   [firstPropertyName, "Hello World"],
-   [secondPropertyName, "Hallo Welt"]
+   [Constants.firstPropertyName, "Hello World"],
+   [Constants.secondPropertyName, "Hallo Welt"]
 ]);
 
 var secondTestDataTableObject = new TableObject();
 secondTestDataTableObject.Uuid = "8d29f002-9511-407b-8289-5ebdcb5a5559";
 secondTestDataTableObject.Properties = new Map([
-   [firstPropertyName, "Table"],
-   [secondPropertyName, "Tabelle"]
+   [Constants.firstPropertyName, "Table"],
+   [Constants.secondPropertyName, "Tabelle"]
 ]);
 
 function clearDatabase(){
@@ -37,13 +30,13 @@ function clearDatabase(){
 describe("Sync function", () => {
    it("should download all table objects from the server", async () => {
       // Arrange
-      Dav.Initialize(false, davClassLibraryTestAppId, [testDataTableId], {
+      Dav.Initialize(false, Constants.davClassLibraryTestAppId, [Constants.testDataTableId], {
          UpdateAllOfTable: () => {},
          UpdateTableObject: () => {},
          DeleteTableObject: () => {},
          ReceiveNotification: () => {}
       });
-      Dav.globals.jwt = davClassLibraryTestUserXTestUserJwt;
+      Dav.globals.jwt = Constants.davClassLibraryTestUserXTestUserJwt;
 
       // Act
       await DataManager.Sync();
@@ -51,14 +44,14 @@ describe("Sync function", () => {
       // Assert
       var tableObjects = await DatabaseOperations.GetAllTableObjects(-1, true);
       assert.equal(2, tableObjects.length);
-      assert.equal(testDataTableId, tableObjects[0].TableId);
-      assert.equal(testDataTableId, tableObjects[1].TableId);
+      assert.equal(Constants.testDataTableId, tableObjects[0].TableId);
+      assert.equal(Constants.testDataTableId, tableObjects[1].TableId);
       assert.equal(firstTestDataTableObject.Uuid, tableObjects[0].Uuid);
       assert.equal(secondTestDataTableObject.Uuid, tableObjects[1].Uuid);
-      assert.equal(firstTestDataTableObject.Properties.get(firstPropertyName), tableObjects[0].Properties.get(firstPropertyName));
-      assert.equal(firstTestDataTableObject.Properties.get(secondPropertyName), tableObjects[0].Properties.get(secondPropertyName));
-      assert.equal(secondTestDataTableObject.Properties.get(firstPropertyName), tableObjects[1].Properties.get(firstPropertyName));
-      assert.equal(secondTestDataTableObject.Properties.get(secondPropertyName), tableObjects[1].Properties.get(secondPropertyName));
+      assert.equal(firstTestDataTableObject.Properties.get(Constants.firstPropertyName), tableObjects[0].Properties.get(Constants.firstPropertyName));
+      assert.equal(firstTestDataTableObject.Properties.get(Constants.secondPropertyName), tableObjects[0].Properties.get(Constants.secondPropertyName));
+      assert.equal(secondTestDataTableObject.Properties.get(Constants.firstPropertyName), tableObjects[1].Properties.get(Constants.firstPropertyName));
+      assert.equal(secondTestDataTableObject.Properties.get(Constants.secondPropertyName), tableObjects[1].Properties.get(Constants.secondPropertyName));
 
       // Tidy up
       clearDatabase();
@@ -66,17 +59,17 @@ describe("Sync function", () => {
 
    it("should remove the table objects that are not on the server", async () => {
       // Arrange
-      Dav.Initialize(false, davClassLibraryTestAppId, [testDataTableId], {
+      Dav.Initialize(false, Constants.davClassLibraryTestAppId, [Constants.testDataTableId], {
          UpdateAllOfTable: () => {},
          UpdateTableObject: () => {},
          DeleteTableObject: () => {},
          ReceiveNotification: () => {}
       });
-      Dav.globals.jwt = davClassLibraryTestUserXTestUserJwt;
+      Dav.globals.jwt = Constants.davClassLibraryTestUserXTestUserJwt;
 
       var deletedTableObject = new TableObject();
       deletedTableObject.UploadStatus = TableObjectUploadStatus.UpToDate;
-      deletedTableObject.TableId = testDataTableId;
+      deletedTableObject.TableId = Constants.testDataTableId;
       await DatabaseOperations.CreateTableObject(deletedTableObject);
 
       // Act
@@ -88,14 +81,14 @@ describe("Sync function", () => {
       assert.isNull(deletedTableObjectFromDatabase);
 
       assert.equal(2, tableObjects.length);
-      assert.equal(testDataTableId, tableObjects[0].TableId);
-      assert.equal(testDataTableId, tableObjects[1].TableId);
+      assert.equal(Constants.testDataTableId, tableObjects[0].TableId);
+      assert.equal(Constants.testDataTableId, tableObjects[1].TableId);
       assert.equal(firstTestDataTableObject.Uuid, tableObjects[0].Uuid);
       assert.equal(secondTestDataTableObject.Uuid, tableObjects[1].Uuid);
-      assert.equal(firstTestDataTableObject.Properties.get(firstPropertyName), tableObjects[0].Properties.get(firstPropertyName));
-      assert.equal(firstTestDataTableObject.Properties.get(secondPropertyName), tableObjects[0].Properties.get(secondPropertyName));
-      assert.equal(secondTestDataTableObject.Properties.get(firstPropertyName), tableObjects[1].Properties.get(firstPropertyName));
-      assert.equal(secondTestDataTableObject.Properties.get(secondPropertyName), tableObjects[1].Properties.get(secondPropertyName));
+      assert.equal(firstTestDataTableObject.Properties.get(Constants.firstPropertyName), tableObjects[0].Properties.get(Constants.firstPropertyName));
+      assert.equal(firstTestDataTableObject.Properties.get(Constants.secondPropertyName), tableObjects[0].Properties.get(Constants.secondPropertyName));
+      assert.equal(secondTestDataTableObject.Properties.get(Constants.firstPropertyName), tableObjects[1].Properties.get(Constants.firstPropertyName));
+      assert.equal(secondTestDataTableObject.Properties.get(Constants.secondPropertyName), tableObjects[1].Properties.get(Constants.secondPropertyName));
 
       // Tidy up
       clearDatabase();
@@ -103,20 +96,20 @@ describe("Sync function", () => {
 
    it("should update only the table objects with a new etag", async () => {
       // Arrange
-      Dav.Initialize(false, davClassLibraryTestAppId, [testDataTableId], {
+      Dav.Initialize(false, Constants.davClassLibraryTestAppId, [Constants.testDataTableId], {
          UpdateAllOfTable: () => {},
          UpdateTableObject: () => {},
          DeleteTableObject: () => {},
          ReceiveNotification: () => {}
       });
-      Dav.globals.jwt = davClassLibraryTestUserXTestUserJwt;
+      Dav.globals.jwt = Constants.davClassLibraryTestUserXTestUserJwt;
 
       await DataManager.Sync();
 
       // Change the etag so that it downloads the table object again
       var secondTableObjectFromDatabase = await DatabaseOperations.GetTableObject(secondTestDataTableObject.Uuid);
       var oldEtag = secondTableObjectFromDatabase.Etag;
-      secondTableObjectFromDatabase.Properties.set(firstPropertyName, "blablabla");
+      secondTableObjectFromDatabase.Properties.set(Constants.firstPropertyName, "blablabla");
       secondTableObjectFromDatabase.Etag = "blablabla";
 
       // Act
@@ -125,7 +118,7 @@ describe("Sync function", () => {
       // Assert
       var secondTableObjectFromDatabase2 = await DatabaseOperations.GetTableObject(secondTestDataTableObject.Uuid);
       assert.equal(oldEtag, secondTableObjectFromDatabase2.Etag);
-      assert.equal(secondTestDataTableObject.Properties.get(firstPropertyName), secondTableObjectFromDatabase2.Properties.get(firstPropertyName));
+      assert.equal(secondTestDataTableObject.Properties.get(Constants.firstPropertyName), secondTableObjectFromDatabase2.Properties.get(Constants.firstPropertyName));
 
       // Tidy up
       clearDatabase();
@@ -135,19 +128,19 @@ describe("Sync function", () => {
 describe("SyncPush function", () => {
    it("should upload created table objects", async () => {
       // Arrange
-      Dav.Initialize(false, davClassLibraryTestAppId, [testDataTableId], {
+      Dav.Initialize(false, Constants.davClassLibraryTestAppId, [Constants.testDataTableId], {
          UpdateAllOfTable: () => {},
          UpdateTableObject: () => {},
          DeleteTableObject: () => {},
          ReceiveNotification: () => {}
       });
-      Dav.globals.jwt = davClassLibraryTestUserXTestUserJwt;
+      Dav.globals.jwt = Constants.davClassLibraryTestUserXTestUserJwt;
 
       var tableObject = new TableObject();
-      tableObject.TableId = testDataTableId;
+      tableObject.TableId = Constants.testDataTableId;
       tableObject.Properties = new Map([
-         [firstPropertyName, "Testtest"],
-         [secondPropertyName, "Test"]
+         [Constants.firstPropertyName, "Testtest"],
+         [Constants.secondPropertyName, "Test"]
       ]);
       await DatabaseOperations.CreateTableObject(tableObject);
 
@@ -158,8 +151,8 @@ describe("SyncPush function", () => {
       // Get the table object from the server
       var tableObjectFromServer = await GetTableObjectFromServer(tableObject.Uuid);
       assert.isNotNull(tableObjectFromServer);
-      assert.equal(tableObject.Properties.get(firstPropertyName), tableObjectFromServer.Properties.get(firstPropertyName));
-      assert.equal(tableObject.Properties.get(secondPropertyName), tableObjectFromServer.Properties.get(secondPropertyName));
+      assert.equal(tableObject.Properties.get(Constants.firstPropertyName), tableObjectFromServer.Properties.get(Constants.firstPropertyName));
+      assert.equal(tableObject.Properties.get(Constants.secondPropertyName), tableObjectFromServer.Properties.get(Constants.secondPropertyName));
       
       var tableObjectFromDatabase = await DatabaseOperations.GetTableObject(tableObject.Uuid);
       assert.equal(TableObjectUploadStatus.UpToDate, tableObjectFromDatabase.UploadStatus);
@@ -171,19 +164,19 @@ describe("SyncPush function", () => {
 
    it("should upload updated table objects", async () => {
       // Arrange
-      Dav.Initialize(false, davClassLibraryTestAppId, [testDataTableId], {
+      Dav.Initialize(false, Constants.davClassLibraryTestAppId, [Constants.testDataTableId], {
          UpdateAllOfTable: () => {},
          UpdateTableObject: () => {},
          DeleteTableObject: () => {},
          ReceiveNotification: () => {}
       });
-      Dav.globals.jwt = davClassLibraryTestUserXTestUserJwt;
+      Dav.globals.jwt = Constants.davClassLibraryTestUserXTestUserJwt;
 
       await DataManager.Sync();
       var newPropertyValue = "testtest";
 
       var tableObject = await DatabaseOperations.GetTableObject(firstTestDataTableObject.Uuid);
-      tableObject.Properties.set(firstPropertyName, newPropertyValue);
+      tableObject.Properties.set(Constants.firstPropertyName, newPropertyValue);
       tableObject.UploadStatus = TableObjectUploadStatus.Updated;
       await DatabaseOperations.UpdateTableObject(tableObject);
       
@@ -195,10 +188,10 @@ describe("SyncPush function", () => {
       assert.equal(TableObjectUploadStatus.UpToDate, tableObjectFromDatabase.UploadStatus);
 
       var tableObjectFromServer = await GetTableObjectFromServer(tableObject.Uuid);
-      assert.equal(newPropertyValue, tableObjectFromServer.Properties.get(firstPropertyName));
+      assert.equal(newPropertyValue, tableObjectFromServer.Properties.get(Constants.firstPropertyName));
 
       // Tidy up
-      tableObjectFromDatabase.Properties.set(firstPropertyName, firstTestDataTableObject.Properties.get(firstPropertyName));
+      tableObjectFromDatabase.Properties.set(Constants.firstPropertyName, firstTestDataTableObject.Properties.get(Constants.firstPropertyName));
       tableObjectFromDatabase.UploadStatus = TableObjectUploadStatus.Updated;
       await DatabaseOperations.UpdateTableObject(tableObjectFromDatabase);
       await DataManager.SyncPush();
@@ -208,19 +201,19 @@ describe("SyncPush function", () => {
 
    it("should upload deleted table objects", async () => {
       // Arrange
-      Dav.Initialize(false, davClassLibraryTestAppId, [testDataTableId], {
+      Dav.Initialize(false, Constants.davClassLibraryTestAppId, [Constants.testDataTableId], {
          UpdateAllOfTable: () => {},
          UpdateTableObject: () => {},
          DeleteTableObject: () => {},
          ReceiveNotification: () => {}
       });
-      Dav.globals.jwt = davClassLibraryTestUserXTestUserJwt;
+      Dav.globals.jwt = Constants.davClassLibraryTestUserXTestUserJwt;
 
       var tableObject = new TableObject();
-      tableObject.TableId = testDataTableId;
+      tableObject.TableId = Constants.testDataTableId;
       tableObject.Properties = new Map([
-         [firstPropertyName, "blabla"],
-         [secondPropertyName, "testtest"]
+         [Constants.firstPropertyName, "blabla"],
+         [Constants.secondPropertyName, "testtest"]
       ]);
       await DatabaseOperations.CreateTableObject(tableObject);
 
@@ -246,17 +239,17 @@ describe("SyncPush function", () => {
 
    it("should delete updated table objects that do not exist on the server", async () => {
       // Arrange
-      Dav.Initialize(false, davClassLibraryTestAppId, [testDataTableId], {
+      Dav.Initialize(false, Constants.davClassLibraryTestAppId, [Constants.testDataTableId], {
          UpdateAllOfTable: () => {},
          UpdateTableObject: () => {},
          DeleteTableObject: () => {},
          ReceiveNotification: () => {}
       });
-      Dav.globals.jwt = davClassLibraryTestUserXTestUserJwt;
+      Dav.globals.jwt = Constants.davClassLibraryTestUserXTestUserJwt;
 
       // Save a table object with upload status updated in the database and run SyncPush
       var tableObject = new TableObject();
-      tableObject.TableId = testDataTableId;
+      tableObject.TableId = Constants.testDataTableId;
       tableObject.UploadStatus = TableObjectUploadStatus.Updated;
 
       await DatabaseOperations.CreateTableObject(tableObject);
@@ -271,17 +264,17 @@ describe("SyncPush function", () => {
 
    it("should delete deleted table objects that do not exist on the server", async () => {
       // Arrange
-      Dav.Initialize(false, davClassLibraryTestAppId, [testDataTableId], {
+      Dav.Initialize(false, Constants.davClassLibraryTestAppId, [Constants.testDataTableId], {
          UpdateAllOfTable: () => {},
          UpdateTableObject: () => {},
          DeleteTableObject: () => {},
          ReceiveNotification: () => {}
       });
-      Dav.globals.jwt = davClassLibraryTestUserXTestUserJwt;
+      Dav.globals.jwt = Constants.davClassLibraryTestUserXTestUserJwt;
 
       // Save a table object with upload status deleted in the database and run SyncPush
       var tableObject = new TableObject();
-      tableObject.TableId = testDataTableId;
+      tableObject.TableId = Constants.testDataTableId;
       tableObject.UploadStatus = TableObjectUploadStatus.Deleted;
 
       await DatabaseOperations.CreateTableObject(tableObject);
@@ -297,7 +290,7 @@ describe("SyncPush function", () => {
    async function GetTableObjectFromServer(uuid: string): Promise<TableObject>{
       try{
          var response = await axios.get(Dav.globals.apiBaseUrl + "apps/object/" + uuid, {
-            headers: {'Authorization': davClassLibraryTestUserXTestUserJwt}
+            headers: {'Authorization': Constants.davClassLibraryTestUserXTestUserJwt}
          });
    
          var tableObject = new TableObject();
@@ -319,7 +312,7 @@ describe("SyncPush function", () => {
          var response = await axios({
             method: 'delete',
             url: Dav.globals.apiBaseUrl + "apps/object/" + uuid,
-            headers: { 'Authorization': davClassLibraryTestUserXTestUserJwt }
+            headers: { 'Authorization': Constants.davClassLibraryTestUserXTestUserJwt }
          });
    
          return {ok: true, message: response.data};
