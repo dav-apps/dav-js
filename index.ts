5cb0da0145f6a0a6852368702a7628fdc5ab54b7
David <dav2070@web.de>
2019-09-28 19:38:26 +0200

    Expose new functions, classes and interfaces

--- index.ts
+++ index.ts
@@ -1,5 +1,33 @@
-export { Init } from "./lib/Dav";
-export { DavUser, ShowLoginPage, ShowSignupPage, DavEnvironment } from "./lib/models/DavUser";
-export { TableObject, Property, generateUUID } from './lib/models/TableObject';
-export { GetTableObject, GetAllTableObjects } from './lib/providers/DatabaseOperations';
-export { Log, SubscribePushNotifications, UnsubscribePushNotifications, CreateNotification, GetNotification, UpdateNotification, DeleteNotification } from './lib/providers/DataManager';
+export { 
+	Init, 
+	InitStatic,
+	ApiResponse,
+	ApiErrorResponse,
+	ApiError
+} from "./lib/Dav";
+export { 
+	DavUser, 
+	ShowLoginPage, 
+	ShowSignupPage, 
+	DavEnvironment
+} from "./lib/models/DavUser";
+export { 
+	TableObject, 
+	Property, 
+	generateUUID
+} from './lib/models/TableObject';
+export { 
+	GetTableObject, 
+	GetAllTableObjects
+} from './lib/providers/DatabaseOperations';
+export { 
+	Log, 
+	SubscribePushNotifications, 
+	UnsubscribePushNotifications, 
+	CreateNotification, 
+	GetNotification, 
+	UpdateNotification, 
+	DeleteNotification
+} from './lib/providers/DataManager';
+export { Auth } from './lib/models/Auth';
+export * from './lib/providers/UsersController';
