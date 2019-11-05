f1b34b345ac6099230a2b30e4731ec7ddf4f57bf
David <dav2070@web.de>
2019-11-05 21:51:04 +0100

    Add GetApp function and tests

--- lib/providers/AppsController.ts
+++ lib/providers/AppsController.ts
@@ -2,6 +2,46 @@ import * as axios from 'axios';
 import { Dav, ApiResponse, ApiErrorResponse, ConvertHttpResponseToErrorResponse } from '../Dav';
 import { Auth } from '../models/Auth';
 import { App, ConvertObjectArrayToApps } from '../models/App';
+import { ConvertObjectArrayToTables } from '../models/Table';
+import { ConvertObjectArrayToEvents } from '../models/Event';
+
+export async function GetApp(jwt: string, id: number) : Promise<ApiResponse<App> | ApiErrorResponse>{
+	let url = `${Dav.apiBaseUrl}/apps/app/${id}`;
+
+	try{
+		let response = await axios.default({
+			method: 'get',
+			url,
+			headers: {
+				Authorization: jwt
+			}
+		});
+
+		return {
+			status: response.status,
+			data: new App(
+				response.data.id,
+				response.data.name,
+				response.data.description,
+				response.data.published,
+				response.data.link_web,
+				response.data.link_play,
+				response.data.link_windows,
+				null,
+				ConvertObjectArrayToTables(response.data.tables),
+				ConvertObjectArrayToEvents(response.data.events)
+			)
+		}
+	}catch(error){
+		if(error.response){
+			// Api error
+			return ConvertHttpResponseToErrorResponse(error.response);
+		}else{
+			// Javascript error
+			return {status: -1, errors: []};
+		}
+	}
+}
 
 export async function GetAllApps(auth: Auth) : Promise<(ApiResponse<App[]> | ApiErrorResponse)>{
 	let url = `${Dav.apiBaseUrl}/apps/apps/all`;
--- test/providers/AppsController.spec.ts
+++ test/providers/AppsController.spec.ts
@@ -5,7 +5,9 @@ import { Dav, InitStatic, ApiResponse, ApiErrorResponse } from '../../lib/Dav';
 import { DavEnvironment } from '../../lib/models/DavUser';
 import { Auth } from '../../lib/models/Auth';
 import { App } from '../../lib/models/App';
-import { GetAllApps } from '../../lib/providers/AppsController';
+import { GetAllApps, GetApp } from '../../lib/providers/AppsController';
+import { Table } from '../../lib/models/Table';
+import { Event } from '../../lib/models/Event';
 
 beforeEach(() => {
 	moxios.install();
@@ -19,6 +21,146 @@ afterEach(() => {
 const devApiKey = "eUzs3PQZYweXvumcWvagRHjdUroGe5Mo7kN1inHm";
 const devSecretKey = "Stac8pRhqH0CSO5o9Rxqjhu7vyVp4PINEMJumqlpvRQai4hScADamQ";
 const devUuid = "d133e303-9dbb-47db-9531-008b20e5aae8";
+
+describe("GetApp function", () => {
+	it("should call getApp endpoint", async () => {
+		// Arrange
+		let appId = 42;
+		let url = `${Dav.apiBaseUrl}/apps/app/${appId}`;
+		let jwt = "jwtjwtjwtjwtjwtjwt";
+
+		let expectedResult: ApiResponse<App> = {
+			status: 200,
+			data: new App(
+				4,
+				"TestApp",
+				"TestApp is the best app!",
+				true,
+				null,
+				null,
+				"https://example.com",
+				null,
+				[
+					new Table(8, 4),
+					new Table(12, 4)
+				],
+				[
+					new Event(12, 4, "visit"),
+					new Event(13, 4, "login")
+				]
+			)
+		}
+
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent();
+
+			// Assert for the request
+			assert.equal(request.config.url, url);
+			assert.equal(request.config.method, 'get');
+			assert.equal(request.config.headers.Authorization, jwt);
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					id: expectedResult.data.Id,
+					name: expectedResult.data.Name,
+					description: expectedResult.data.Description,
+					published: expectedResult.data.Published,
+					link_web: expectedResult.data.LinkWeb,
+					link_play: expectedResult.data.LinkPlay,
+					link_windows: expectedResult.data.LinkWindows,
+					tables: [
+						{
+							id: expectedResult.data.Tables[0].Id,
+							app_id: expectedResult.data.Tables[0].AppId
+						},
+						{
+							id: expectedResult.data.Tables[1].Id,
+							app_id: expectedResult.data.Tables[1].AppId
+						}
+					],
+					events: [
+						{
+							id: expectedResult.data.Events[0].Id,
+							app_id: expectedResult.data.Events[0].AppId,
+							name: expectedResult.data.Events[0].Name
+						},
+						{
+							id: expectedResult.data.Events[1].Id,
+							app_id: expectedResult.data.Events[1].AppId,
+							name: expectedResult.data.Events[1].Name
+						}
+					]
+				}
+			});
+		});
+
+		// Act
+		let result = await GetApp(jwt, appId) as ApiResponse<App>;
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status);
+		assert.equal(result.data.Id, expectedResult.data.Id);
+		assert.equal(result.data.Name, expectedResult.data.Name);
+		assert.equal(result.data.Description, expectedResult.data.Description);
+		assert.equal(result.data.Published, expectedResult.data.Published);
+		assert.equal(result.data.LinkWeb, expectedResult.data.LinkWeb);
+		assert.equal(result.data.LinkPlay, expectedResult.data.LinkPlay);
+		assert.equal(result.data.LinkWindows, expectedResult.data.LinkWindows);
+		assert.equal(result.data.UsedStorage, expectedResult.data.UsedStorage);
+		assert.equal(result.data.Tables[0].Id, expectedResult.data.Tables[0].Id);
+		assert.equal(result.data.Tables[0].AppId, expectedResult.data.Tables[0].AppId);
+		assert.equal(result.data.Tables[1].Id, expectedResult.data.Tables[1].Id);
+		assert.equal(result.data.Tables[1].AppId, expectedResult.data.Tables[1].AppId);
+		assert.equal(result.data.Events[0].Id, expectedResult.data.Events[0].Id);
+		assert.equal(result.data.Events[0].AppId, expectedResult.data.Events[0].AppId);
+		assert.equal(result.data.Events[0].Name, expectedResult.data.Events[0].Name);
+		assert.equal(result.data.Events[1].Id, expectedResult.data.Events[1].Id);
+		assert.equal(result.data.Events[1].AppId, expectedResult.data.Events[1].AppId);
+		assert.equal(result.data.Events[1].Name, expectedResult.data.Events[1].Name);
+	});
+
+	it("should call getApp endpoint with error", async () => {
+		// Arrange
+		let appId = 42;
+		let url = `${Dav.apiBaseUrl}/apps/app/${appId}`;
+		let jwt = "jwtjwtjwtjwtjwtjwt";
+
+		let expectedResult: ApiErrorResponse = {
+			status: 404,
+			errors: [{
+				code: 2803,
+				message: "Resource does not exist: App"
+			}]
+		}
+
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent();
+
+			// Assert for the request
+			assert.equal(request.config.url, url);
+			assert.equal(request.config.method, 'get');
+			assert.equal(request.config.headers.Authorization, jwt);
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					errors: [
+						[expectedResult.errors[0].code, expectedResult.errors[0].message]
+					]
+				}
+			});
+		});
+
+		// Act
+		let result = await GetApp(jwt, appId) as ApiErrorResponse;
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status);
+		assert.equal(result.errors[0].code, expectedResult.errors[0].code);
+		assert.equal(result.errors[0].message, expectedResult.errors[0].message);
+	});
+});
 
 describe("GetAllApps function", () => {
 	it("should call getAllApps endpoint", async () => {
