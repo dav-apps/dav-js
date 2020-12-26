fd9d4c9b2bc040380183bfadbefd1917d6a9115c
David <dav2070@web.de>
2020-12-26 23:32:54 +0100

    Update App table for new api
    - Remove event atttributes and references
    - Rename link_web to web_link, link_play to google_play_link and link_windows to microsoft_store_link

--- lib/models/App.ts
+++ lib/models/App.ts
@@ -1,43 +1,40 @@
-import { Table, ConvertObjectArrayToTables } from './Table';
-import { Event, ConvertObjectArrayToEvents } from './Event';
-import { Api, ConvertObjectArrayToApis } from './Api';
+import { Table, ConvertObjectArrayToTables } from './Table'
+import { Api, ConvertObjectArrayToApis } from './Api'
 
-export class App{
+export class App {
 	constructor(
 		public Id: number,
 		public Name: string,
 		public Description: string,
 		public Published: boolean,
-		public LinkWeb: string,
-		public LinkPlay: string,
-		public LinkWindows: string,
+		public WebLink: string,
+		public GooglePlayLink: string,
+		public MicrosoftStoreLink: string,
 		public UsedStorage: number = 0,
 		public Tables: Table[] = [],
-		public Events: Event[] = [],
 		public Apis: Api[] = []
-	){}
+	) { }
 }
 
-export function ConvertObjectArrayToApps(objArray: any[]) : App[]{
-	let apps: App[] = [];
+export function ConvertObjectArrayToApps(objArray: any[]): App[] {
+	let apps: App[] = []
 
-	if(objArray){
-		for(let obj of objArray){
+	if (objArray) {
+		for (let obj of objArray) {
 			apps.push(new App(
 				obj.id,
 				obj.name,
 				obj.description,
 				obj.published,
-				obj.link_web,
-				obj.link_play,
-				obj.link_windows,
+				obj.web_link,
+				obj.google_play_link,
+				obj.microsft_store_link,
 				obj.used_storage,
 				ConvertObjectArrayToTables(obj.tables),
-				ConvertObjectArrayToEvents(obj.events),
 				ConvertObjectArrayToApis(obj.apis)
-			));
+			))
 		}
 	}
 
-	return apps;
+	return apps
 }
