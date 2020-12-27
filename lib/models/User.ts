aca4934e52d4358bc7429dd16b97824f3b3768cb
David <dav2070@web.de>
2020-12-27 13:23:58 +0100

    Add User class

---
+++ lib/models/User.ts
@@ -0,0 +1,20 @@
+import { Plan, SubscriptionStatus } from '../types'
+import { App } from './App'
+
+export class User{
+	constructor(
+		public Id: number,
+		public Email: string,
+		public FirstName: string,
+		public Confirmed: boolean,
+		public TotalStorage: number,
+		public UsedStorage: number,
+		public StripeCustomerId: string,
+		public Plan: Plan,
+		public SubscriptionStatus: SubscriptionStatus,
+		public PeriodEnd: Date,
+		public Dev: boolean,
+		public Provider: boolean,
+		public Apps: App[]
+	){}
+}
