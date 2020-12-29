e319914deff3f298fec8d3f41ef412c8ba86911d
David <dav2070@web.de>
2020-12-30 00:48:36 +0100

    Add errorCodes file with the used error codes as constants

---
+++ lib/errorCodes.ts
@@ -0,0 +1,7 @@
+export const ActionNotAllowed = 1103
+
+export const UuidAlreadyTaken = 2702
+
+export const TableObjectDoesNotExist = 2805
+export const SessionDoesNotExist = 2806
+export const TableObjectUserAccessDoesNotExist = 2807
