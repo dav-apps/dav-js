17aa03f17a7ec249c0106bfb906f6837e5d54469
David <dav2070@web.de>
2021-01-11 17:53:33 +0100

    Add CreateSession function

--- lib/controllers/SessionsController.ts
+++ lib/controllers/SessionsController.ts
@@ -1,7 +1,53 @@
 import * as axios from 'axios'
 import { Dav } from '../Dav'
+import { Auth } from '../models/Auth'
 import { ApiErrorResponse, ApiResponse } from '../types'
 import { ConvertErrorToApiErrorResponse } from '../utils'
+
+export interface CreateSessionResponseData{
+	jwt: string
+}
+
+export async function CreateSession(params: {
+	auth: Auth,
+	email: string,
+	password: string,
+	appId: number,
+	apiKey: string,
+	deviceName?: string,
+	deviceType?: string,
+	deviceOs?: string
+}): Promise<ApiResponse<CreateSessionResponseData> | ApiErrorResponse>{
+	try {
+		let data = {
+			email: params.email,
+			password: params.password,
+			app_id: params.appId,
+			api_key: params.apiKey
+		}
+		if (params.deviceName != null) data["device_name"] = params.deviceName
+		if (params.deviceType != null) data["device_type"] = params.deviceType
+		if (params.deviceOs != null) data["device_os"] = params.deviceOs
+
+		let response = await axios.default({
+			method: 'post',
+			url: `${Dav.apiBaseUrl}/session`,
+			headers: {
+				Authorization: params.auth.token
+			},
+			data
+		})
+
+		return {
+			status: response.status,
+			data: {
+				jwt: response.data.jwt
+			}
+		}
+	} catch (error) {
+		return ConvertErrorToApiErrorResponse(error)
+	}
+}
 
 export async function DeleteSession(params: {
 	jwt: string
