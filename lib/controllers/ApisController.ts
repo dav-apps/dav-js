5b27fb09fd297dd55d796e0c97c8b65bf7e0faac
David <dav2070@web.de>
2021-01-14 17:44:31 +0100

    Add GetApi function and tests

--- lib/controllers/ApisController.ts
+++ lib/controllers/ApisController.ts
@@ -3,6 +3,9 @@ import { Dav } from '../Dav'
 import { ApiResponse, ApiErrorResponse } from '../types'
 import { ConvertErrorToApiErrorResponse } from '../utils'
 import { Api } from '../models/Api'
+import { ConvertObjectArrayToApiEndpoints } from '../models/ApiEndpoint'
+import { ConvertObjectArrayToApiFunctions } from '../models/ApiFunction'
+import { ConvertObjectArrayToApiErrors } from '../models/ApiError'
 
 export async function CreateApi(params: {
 	jwt: string,
@@ -30,6 +33,34 @@ export async function CreateApi(params: {
 				[],
 				[],
 				[]
+			)
+		}
+	} catch (error) {
+		return ConvertErrorToApiErrorResponse(error)
+	}
+}
+
+export async function GetApi(params: {
+	jwt: string,
+	id: number
+}): Promise<ApiResponse<Api> | ApiErrorResponse>{
+	try {
+		let response = await axios.default({
+			method: 'get',
+			url: `${Dav.apiBaseUrl}/api/${params.id}`,
+			headers: {
+				Authorization: params.jwt
+			}
+		})
+
+		return {
+			status: response.status,
+			data: new Api(
+				response.data.id,
+				response.data.name,
+				ConvertObjectArrayToApiEndpoints(response.data.endpoints),
+				ConvertObjectArrayToApiFunctions(response.data.functions),
+				ConvertObjectArrayToApiErrors(response.data.errors)
 			)
 		}
 	} catch (error) {
--- test/controllers/ApisController.spec.ts
+++ test/controllers/ApisController.spec.ts
@@ -3,7 +3,10 @@ import * as moxios from 'moxios'
 import { Dav } from '../../lib/Dav'
 import { ApiResponse, ApiErrorResponse } from '../../lib/types'
 import { Api } from '../../lib/models/Api'
-import { CreateApi } from '../../lib/controllers/ApisController'
+import { CreateApi, GetApi } from '../../lib/controllers/ApisController'
+import { ApiEndpoint } from '../../lib/models/ApiEndpoint'
+import { ApiFunction } from '../../lib/models/ApiFunction'
+import { ApiError } from '../../lib/models/ApiError'
 
 beforeEach(() => {
 	moxios.install()
@@ -121,6 +124,169 @@ describe("CreateApi function", () => {
 			jwt,
 			appId,
 			name
+		}) as ApiErrorResponse
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status)
+		assert.equal(result.errors[0].code, expectedResult.errors[0].code)
+		assert.equal(result.errors[0].message, expectedResult.errors[0].message)
+	})
+})
+
+describe("GetApi function", () => {
+	it("should call getApi endpoint", async () => {
+		// Arrange
+		let id = 12
+		let appId = 2
+		let name = "TestApi"
+		let apiEndpointId = 2
+		let apiEndpointPath = "test/path/:id"
+		let apiEndpointMethod = "GET"
+		let apiEndpointCaching = true
+		let apiFunctionId = 24
+		let apiFunctionName = "TestFunction"
+		let apiFunctionParams = ["a", "b"]
+		let apiErrorId = 46
+		let apiErrorCode = 1111
+		let apiErrorMessage = "Test ErrorMessage"
+
+		let jwt = "idshfhif9w8h39f24hfw"
+		let url = `${Dav.apiBaseUrl}/api/${id}`
+
+		let expectedResult: ApiResponse<Api> = {
+			status: 200,
+			data: new Api(
+				id,
+				name,
+				[
+					new ApiEndpoint(
+						apiEndpointId,
+						apiEndpointPath,
+						apiEndpointMethod,
+						null,
+						apiEndpointCaching
+					)
+				],
+				[
+					new ApiFunction(
+						apiFunctionId,
+						apiFunctionName,
+						apiFunctionParams,
+						null
+					)
+				],
+				[
+					new ApiError(
+						apiErrorId,
+						apiErrorCode,
+						apiErrorMessage
+					)
+				]
+			)
+		}
+
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'get')
+			assert.equal(request.config.headers.Authorization, jwt)
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					id,
+					appId,
+					name,
+					endpoints: [{
+						id: apiEndpointId,
+						path: apiEndpointPath,
+						method: apiEndpointMethod,
+						caching: apiEndpointCaching
+					}],
+					functions: [{
+						id: apiFunctionId,
+						name: apiFunctionName,
+						params: apiFunctionParams.join(',')
+					}],
+					errors: [{
+						id: apiErrorId,
+						code: apiErrorCode,
+						message: apiErrorMessage
+					}]
+				}
+			})
+		})
+
+		// Act
+		let result = await GetApi({
+			jwt,
+			id
+		}) as ApiResponse<Api>
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status)
+		assert.equal(result.data.Id, expectedResult.data.Id)
+		assert.equal(result.data.Name, expectedResult.data.Name)
+
+		assert.equal(result.data.Endpoints.length, 1)
+		assert.equal(result.data.Endpoints[0].Id, expectedResult.data.Endpoints[0].Id)
+		assert.equal(result.data.Endpoints[0].Path, expectedResult.data.Endpoints[0].Path)
+		assert.equal(result.data.Endpoints[0].Method, expectedResult.data.Endpoints[0].Method)
+		assert.equal(result.data.Endpoints[0].Caching, expectedResult.data.Endpoints[0].Caching)
+		assert.equal(result.data.Endpoints[0].Commands, expectedResult.data.Endpoints[0].Commands)
+		
+		assert.equal(result.data.Functions.length, 1)
+		assert.equal(result.data.Functions[0].Id, expectedResult.data.Functions[0].Id)
+		assert.equal(result.data.Functions[0].Name, expectedResult.data.Functions[0].Name)
+		assert.equal(result.data.Functions[0].Params.join(','), expectedResult.data.Functions[0].Params.join(','))
+		assert.equal(result.data.Functions[0].Commands, expectedResult.data.Functions[0].Commands)
+
+		assert.equal(result.data.Errors.length, 1)
+		assert.equal(result.data.Errors[0].Id, expectedResult.data.Errors[0].Id)
+		assert.equal(result.data.Errors[0].Code, expectedResult.data.Errors[0].Code)
+		assert.equal(result.data.Errors[0].Message, expectedResult.data.Errors[0].Message)
+	})
+
+	it("should call getApi endpoint with error", async () => {
+		// Arrange
+		let id = 12
+
+		let jwt = "idshfhif9w8h39f24hfw"
+		let url = `${Dav.apiBaseUrl}/api/${id}`
+
+		let expectedResult: ApiErrorResponse = {
+			status: 403,
+			errors: [{
+				code: 1103,
+				message: "Action not allowed"
+			}]
+		}
+
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'get')
+			assert.equal(request.config.headers.Authorization, jwt)
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					errors: [{
+						code: expectedResult.errors[0].code,
+						message: expectedResult.errors[0].message
+					}]
+				}
+			})
+		})
+
+		// Act
+		let result = await GetApi({
+			jwt,
+			id
 		}) as ApiErrorResponse
 
 		// Assert for the response
