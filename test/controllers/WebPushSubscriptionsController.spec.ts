36f21a307c1e8092869736ae7e12f91ae825c481
David <dav2070@web.de>
2021-01-27 00:31:22 +0100

    Add tests for WebPushSubscription functions

---
+++ test/controllers/WebPushSubscriptionsController.spec.ts
@@ -0,0 +1,140 @@
+import { assert } from 'chai'
+import * as moxios from 'moxios'
+import { Dav } from '../../lib/Dav'
+import { ApiResponse, ApiErrorResponse } from '../../lib/types'
+import { WebPushSubscription } from '../../lib/models/WebPushSubscription'
+import { CreateWebPushSubscription } from '../../lib/controllers/WebPushSubscriptionsController'
+
+beforeEach(() => {
+	moxios.install()
+})
+
+afterEach(() => {
+	moxios.uninstall()
+})
+
+describe("CreateWebPushSubscription function", () => {
+	it("should call createWebPushSubscription endpoint", async () => {
+		// Arrange
+		let uuid = "3853277f-3633-4092-998d-15b928345924"
+		let endpoint = "https://bla.microsoft.com/test"
+		let p256dh = "asdasfpadgijsdghosidg"
+		let auth = "uiafuihdahuiaf"
+
+		let accessToken = "shiodghsodghsgod"
+		let url = `${Dav.apiBaseUrl}/web_push_subscription`
+
+		let expectedResult: ApiResponse<WebPushSubscription> = {
+			status: 201,
+			data: new WebPushSubscription(
+				uuid,
+				endpoint,
+				p256dh,
+				auth
+			)
+		}
+
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'post')
+			assert.equal(request.config.headers.Authorization, accessToken)
+			assert.include(request.config.headers["Content-Type"], "application/json")
+
+			let data = JSON.parse(request.config.data)
+			assert.equal(data.uuid, uuid)
+			assert.equal(data.endpoint, endpoint)
+			assert.equal(data.p256dh, p256dh)
+			assert.equal(data.auth, auth)
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					id: 12,
+					user_id: 12,
+					uuid,
+					endpoint,
+					p256dh,
+					auth
+				}
+			})
+		})
+
+		// Act
+		let result = await CreateWebPushSubscription({
+			accessToken,
+			uuid,
+			endpoint,
+			p256dh,
+			auth
+		}) as ApiResponse<WebPushSubscription>
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status)
+		assert.equal(result.data.Uuid, expectedResult.data.Uuid)
+		assert.equal(result.data.Endpoint, expectedResult.data.Endpoint)
+		assert.equal(result.data.P256dh, expectedResult.data.P256dh)
+		assert.equal(result.data.Auth, expectedResult.data.Auth)
+	})
+
+	it("should call createWebPushSubscription endpoint with error", async () => {
+		// Arrange
+		let uuid = "3853277f-3633-4092-998d-15b928345924"
+		let endpoint = "https://bla.microsoft.com/test"
+		let p256dh = "asdasfpadgijsdghosidg"
+		let auth = "uiafuihdahuiaf"
+
+		let accessToken = "shiodghsodghsgod"
+		let url = `${Dav.apiBaseUrl}/web_push_subscription`
+
+		let expectedResult: ApiErrorResponse = {
+			status: 403,
+			errors: [{
+				code: 1103,
+				message: "Action not allowed"
+			}]
+		}
+
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'post')
+			assert.equal(request.config.headers.Authorization, accessToken)
+			assert.include(request.config.headers["Content-Type"], "application/json")
+
+			let data = JSON.parse(request.config.data)
+			assert.equal(data.uuid, uuid)
+			assert.equal(data.endpoint, endpoint)
+			assert.equal(data.p256dh, p256dh)
+			assert.equal(data.auth, auth)
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					errors: [{
+						code: expectedResult.errors[0].code,
+						message: expectedResult.errors[0].message
+					}]
+				}
+			})
+		})
+
+		// Act
+		let result = await CreateWebPushSubscription({
+			accessToken,
+			uuid,
+			endpoint,
+			p256dh,
+			auth
+		}) as ApiErrorResponse
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status)
+		assert.equal(result.errors[0].code, expectedResult.errors[0].code)
+		assert.equal(result.errors[0].message, expectedResult.errors[0].message)
+	})
+})
