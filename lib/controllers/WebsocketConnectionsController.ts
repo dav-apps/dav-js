389d98eca8e7574288121c78e3918f556ddb1ed6
David <dav2070@web.de>
2021-01-28 00:30:14 +0100

    Update WebsocketConnection functions to renew the session if necessary
    - Update functions in WebsocketConnectionsController to use HandleApiError for renewing the session if necessary
    - Remove accessToken from the params of the functions and use Dav.accessToken instead
    - Update existing tests and add tests for renewing the session

--- lib/controllers/WebsocketConnectionsController.ts
+++ lib/controllers/WebsocketConnectionsController.ts
@@ -1,21 +1,19 @@
 import * as axios from 'axios'
 import { Dav } from '../Dav'
 import { ApiErrorResponse, ApiResponse } from '../types'
-import { ConvertErrorToApiErrorResponse } from '../utils'
+import { HandleApiError } from '../utils'
 
 export interface WebsocketConnectionResponseData{
 	token: string
 }
 
-export async function CreateWebsocketConnection(params: {
-	accessToken: string
-}): Promise<ApiResponse<WebsocketConnectionResponseData> | ApiErrorResponse> {
+export async function CreateWebsocketConnection(): Promise<ApiResponse<WebsocketConnectionResponseData> | ApiErrorResponse> {
 	try {
 		let response = await axios.default({
 			method: 'post',
 			url: `${Dav.apiBaseUrl}/websocket_connection`,
 			headers: {
-				Authorization: params.accessToken
+				Authorization: Dav.accessToken
 			}
 		})
 
@@ -26,6 +24,12 @@ export async function CreateWebsocketConnection(params: {
 			}
 		}
 	} catch (error) {
-		return ConvertErrorToApiErrorResponse(error)
+		let result = await HandleApiError(error)
+
+		if (typeof result == "string") {
+			return await CreateWebsocketConnection()
+		} else {
+			return result as ApiErrorResponse
+		}
 	}
 }
--- test/controllers/WebsocketConnectionsController.spec.ts
+++ test/controllers/WebsocketConnectionsController.spec.ts
@@ -21,6 +21,7 @@ describe("CreateWebsocketConnection function", () => {
 		let token = "shodghosdgs"
 
 		let accessToken = "asdhioagdioasg"
+		Dav.accessToken = accessToken
 		let url = `${Dav.apiBaseUrl}/websocket_connection`
 
 		let expectedResult: ApiResponse<WebsocketConnectionResponseData> = {
@@ -47,9 +48,7 @@ describe("CreateWebsocketConnection function", () => {
 		})
 
 		// Act
-		let result = await CreateWebsocketConnection({
-			accessToken
-		}) as ApiResponse<WebsocketConnectionResponseData>
+		let result = await CreateWebsocketConnection() as ApiResponse<WebsocketConnectionResponseData>
 
 		// Assert for the response
 		assert.equal(result.status, expectedResult.status)
@@ -58,9 +57,8 @@ describe("CreateWebsocketConnection function", () => {
 
 	it("should call createWebsocketConnection endpoint with error", async () => {
 		// Arrange
-		let token = "shodghosdgs"
-
 		let accessToken = "asdhioagdioasg"
+		Dav.accessToken = accessToken
 		let url = `${Dav.apiBaseUrl}/websocket_connection`
 
 		let expectedResult: ApiErrorResponse = {
@@ -91,13 +89,89 @@ describe("CreateWebsocketConnection function", () => {
 		})
 
 		// Act
-		let result = await CreateWebsocketConnection({
-			accessToken
-		}) as ApiErrorResponse
+		let result = await CreateWebsocketConnection() as ApiErrorResponse
 
 		// Assert for the response
 		assert.equal(result.status, expectedResult.status)
 		assert.equal(result.errors[0].code, expectedResult.errors[0].code)
 		assert.equal(result.errors[0].message, expectedResult.errors[0].message)
+	})
+
+	it("should call createWebsocketConnection endpoint and renew the session", async () => {
+		// Arrange
+		let token = "shodghosdgs"
+
+		let accessToken = "asdhioagdioasg"
+		let newAccessToken = "shiodghiosdhiosdg"
+		Dav.accessToken = accessToken
+		let url = `${Dav.apiBaseUrl}/websocket_connection`
+
+		let expectedResult: ApiResponse<WebsocketConnectionResponseData> = {
+			status: 201,
+			data: {
+				token
+			}
+		}
+
+		// First createWebsocketConnection request
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'post')
+			assert.equal(request.config.headers.Authorization, accessToken)
+
+			request.respondWith({
+				status: 403,
+				response: {
+					errors: [{
+						code: 1602,
+						message: "Action not allowed"
+					}]
+				}
+			})
+		})
+
+		// renewSession request
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, `${Dav.apiBaseUrl}/session/renew`)
+			assert.equal(request.config.method, 'put')
+			assert.equal(request.config.headers.Authorization, accessToken)
+
+			request.respondWith({
+				status: 200,
+				response: {
+					access_token: newAccessToken
+				}
+			})
+		})
+
+		// Second createWebsocketConnection request
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'post')
+			assert.equal(request.config.headers.Authorization, newAccessToken)
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					token
+				}
+			})
+		})
+
+		// Act
+		let result = await CreateWebsocketConnection() as ApiResponse<WebsocketConnectionResponseData>
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status)
+		assert.equal(result.data.token, expectedResult.data.token)
 	})
 })
