685140d50e0bf03e46e26d56098c62616f281e0a
David <dav2070@web.de>
2021-02-02 15:35:51 +0100

    Add PurchasesController with CreatePurchase function and tests

---
+++ lib/controllers/PurchasesController.ts
@@ -0,0 +1,56 @@
+import * as axios from 'axios'
+import { Dav } from '../Dav'
+import { ApiResponse, ApiErrorResponse } from '../types'
+import { HandleApiError } from '../utils'
+import { Currency, Purchase } from '../models/Purchase'
+
+export async function CreatePurchase(params: {
+	tableObjectUuid: string,
+	providerName: string,
+	providerImage: string,
+	productName: string,
+	productImage: string,
+	currency: Currency
+}): Promise<ApiResponse<Purchase> | ApiErrorResponse> {
+	try {
+		let response = await axios.default({
+			method: 'post',
+			url: `${Dav.apiBaseUrl}/table_object/${params.tableObjectUuid}/purchase`,
+			headers: {
+				Authorization: Dav.accessToken
+			},
+			data: {
+				provider_name: params.providerName,
+				provider_image: params.providerImage,
+				product_name: params.productName,
+				product_image: params.productImage,
+				currency: params.currency
+			}
+		})
+
+		return {
+			status: response.status,
+			data: {
+				Id: response.data.id,
+				UserId: response.data.user_id,
+				TableObjectId: response.data.table_object_id,
+				PaymentIntentId: response.data.payment_intent_id,
+				ProviderName: response.data.provider_name,
+				ProviderImage: response.data.provider_image,
+				ProductName: response.data.product_name,
+				ProductImage: response.data.product_image,
+				Price: response.data.price,
+				Currency: response.data.currency,
+				Completed: response.data.completed
+			}
+		}
+	} catch (error) {
+		let result = await HandleApiError(error)
+
+		if (typeof result == "string") {
+			return await CreatePurchase(params)
+		} else {
+			return result as ApiErrorResponse
+		}
+	}
+}
---
+++ test/controllers/PurchasesController.spec.ts
@@ -0,0 +1,315 @@
+import { assert } from 'chai'
+import * as moxios from 'moxios'
+import { Dav } from '../../lib/Dav'
+import { ApiResponse, ApiErrorResponse } from '../../lib/types'
+import { Currency, Purchase } from '../../lib/models/Purchase'
+import { CreatePurchase } from '../../lib/controllers/PurchasesController'
+
+beforeEach(() => {
+	moxios.install()
+})
+
+afterEach(() => {
+	moxios.uninstall()
+})
+
+describe("CreatePurchase function", () => {
+	it("should call createPurchase endpoint", async () => {
+		// Arrange
+		let tableObjectUuid = "28c66e28-f1cd-4765-b38f-e03c0f467ae9"
+		let id = 12
+		let userId = 23
+		let tableObjectId = 34
+		let paymentIntentId = "pi_iasdohafhoasguf"
+		let providerName = "Lemony Snicket"
+		let providerImage = "https://api.pocketlib.app/author/asdasdasd/profile_image"
+		let productName = "A Series of Unfortunate Events - Book the First"
+		let productImage = "https://api.pocketlib.app/store/book/sdfsdfsfddf/cover"
+		let price = 1313
+		let currency: Currency = "eur"
+		let completed = false
+
+		let accessToken = "osdosdiosdf"
+		Dav.accessToken = accessToken
+		let url = `${Dav.apiBaseUrl}/table_object/${tableObjectUuid}/purchase`
+
+		let expectedResult: ApiResponse<Purchase> = {
+			status: 201,
+			data: {
+				Id: id,
+				UserId: userId,
+				TableObjectId: tableObjectId,
+				PaymentIntentId: paymentIntentId,
+				ProviderName: providerName,
+				ProviderImage: providerImage,
+				ProductName: productName,
+				ProductImage: productImage,
+				Price: price,
+				Currency: currency,
+				Completed: completed
+			}
+		}
+
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'post')
+			assert.equal(request.config.headers.Authorization, accessToken)
+			assert.include(request.config.headers["Content-Type"], "application/json")
+
+			let data = JSON.parse(request.config.data)
+			assert.equal(data.provider_name, providerName)
+			assert.equal(data.provider_image, providerImage)
+			assert.equal(data.product_name, productName)
+			assert.equal(data.product_image, productImage)
+			assert.equal(data.currency, currency)
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					id,
+					user_id: userId,
+					table_object_id: tableObjectId,
+					payment_intent_id: paymentIntentId,
+					provider_name: providerName,
+					provider_image: providerImage,
+					product_name: productName,
+					product_image: productImage,
+					price,
+					currency,
+					completed
+				}
+			})
+		})
+
+		// Act
+		let result = await CreatePurchase({
+			tableObjectUuid,
+			providerName,
+			providerImage,
+			productName,
+			productImage,
+			currency
+		}) as ApiResponse<Purchase>
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status)
+		assert.equal(result.data.Id, expectedResult.data.Id)
+		assert.equal(result.data.UserId, expectedResult.data.UserId)
+		assert.equal(result.data.TableObjectId, expectedResult.data.TableObjectId)
+		assert.equal(result.data.ProviderName, expectedResult.data.ProviderName)
+		assert.equal(result.data.ProviderImage, expectedResult.data.ProviderImage)
+		assert.equal(result.data.ProductName, expectedResult.data.ProductName)
+		assert.equal(result.data.ProductImage, expectedResult.data.ProductImage)
+		assert.equal(result.data.Price, expectedResult.data.Price)
+		assert.equal(result.data.Currency, expectedResult.data.Currency)
+		assert.equal(result.data.Completed, expectedResult.data.Completed)
+	})
+
+	it("should call createPurchase endpoint with error", async () => {
+		// Arrange
+		let tableObjectUuid = "28c66e28-f1cd-4765-b38f-e03c0f467ae9"
+		let providerName = "Lemony Snicket"
+		let providerImage = "https://api.pocketlib.app/author/asdasdasd/profile_image"
+		let productName = "A Series of Unfortunate Events - Book the First"
+		let productImage = "https://api.pocketlib.app/store/book/sdfsdfsfddf/cover"
+		let currency: Currency = "eur"
+
+		let accessToken = "osdosdiosdf"
+		Dav.accessToken = accessToken
+		let url = `${Dav.apiBaseUrl}/table_object/${tableObjectUuid}/purchase`
+
+		let expectedResult: ApiErrorResponse = {
+			status: 403,
+			errors: [{
+				code: 1103,
+				message: "Action not allowed"
+			}]
+		}
+
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'post')
+			assert.equal(request.config.headers.Authorization, accessToken)
+			assert.include(request.config.headers["Content-Type"], "application/json")
+
+			let data = JSON.parse(request.config.data)
+			assert.equal(data.provider_name, providerName)
+			assert.equal(data.provider_image, providerImage)
+			assert.equal(data.product_name, productName)
+			assert.equal(data.product_image, productImage)
+			assert.equal(data.currency, currency)
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					errors: [{
+						code: expectedResult.errors[0].code,
+						message: expectedResult.errors[0].message
+					}]
+				}
+			})
+		})
+
+		// Act
+		let result = await CreatePurchase({
+			tableObjectUuid,
+			providerName,
+			providerImage,
+			productName,
+			productImage,
+			currency
+		}) as ApiErrorResponse
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status)
+		assert.equal(result.errors[0].code, expectedResult.errors[0].code)
+		assert.equal(result.errors[0].message, expectedResult.errors[0].message)
+	})
+
+	it("should call createPurchase endpoint and renew the session", async () => {
+		// Arrange
+		let tableObjectUuid = "28c66e28-f1cd-4765-b38f-e03c0f467ae9"
+		let id = 12
+		let userId = 23
+		let tableObjectId = 34
+		let paymentIntentId = "pi_iasdohafhoasguf"
+		let providerName = "Lemony Snicket"
+		let providerImage = "https://api.pocketlib.app/author/asdasdasd/profile_image"
+		let productName = "A Series of Unfortunate Events - Book the First"
+		let productImage = "https://api.pocketlib.app/store/book/sdfsdfsfddf/cover"
+		let price = 1313
+		let currency: Currency = "eur"
+		let completed = false
+
+		let accessToken = "osdosdiosdf"
+		let newAccessToken = "sgiodsfghiosghio"
+		Dav.accessToken = accessToken
+		let url = `${Dav.apiBaseUrl}/table_object/${tableObjectUuid}/purchase`
+
+		let expectedResult: ApiResponse<Purchase> = {
+			status: 201,
+			data: {
+				Id: id,
+				UserId: userId,
+				TableObjectId: tableObjectId,
+				PaymentIntentId: paymentIntentId,
+				ProviderName: providerName,
+				ProviderImage: providerImage,
+				ProductName: productName,
+				ProductImage: productImage,
+				Price: price,
+				Currency: currency,
+				Completed: completed
+			}
+		}
+
+		// First createPurchase request
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'post')
+			assert.equal(request.config.headers.Authorization, accessToken)
+			assert.include(request.config.headers["Content-Type"], "application/json")
+
+			let data = JSON.parse(request.config.data)
+			assert.equal(data.provider_name, providerName)
+			assert.equal(data.provider_image, providerImage)
+			assert.equal(data.product_name, productName)
+			assert.equal(data.product_image, productImage)
+			assert.equal(data.currency, currency)
+
+			request.respondWith({
+				status: 403,
+				response: {
+					errors: [{
+						code: 1602,
+						message: "Access token must be renewed"
+					}]
+				}
+			})
+		})
+
+		// renewSession request
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, `${Dav.apiBaseUrl}/session/renew`)
+			assert.equal(request.config.method, 'put')
+			assert.equal(request.config.headers.Authorization, accessToken)
+
+			request.respondWith({
+				status: 200,
+				response: {
+					access_token: newAccessToken
+				}
+			})
+		})
+
+		// Second createPurchase request
+		moxios.wait(() => {
+			let request = moxios.requests.mostRecent()
+
+			// Assert for the request
+			assert.equal(request.config.url, url)
+			assert.equal(request.config.method, 'post')
+			assert.equal(request.config.headers.Authorization, newAccessToken)
+			assert.include(request.config.headers["Content-Type"], "application/json")
+
+			let data = JSON.parse(request.config.data)
+			assert.equal(data.provider_name, providerName)
+			assert.equal(data.provider_image, providerImage)
+			assert.equal(data.product_name, productName)
+			assert.equal(data.product_image, productImage)
+			assert.equal(data.currency, currency)
+
+			request.respondWith({
+				status: expectedResult.status,
+				response: {
+					id,
+					user_id: userId,
+					table_object_id: tableObjectId,
+					payment_intent_id: paymentIntentId,
+					provider_name: providerName,
+					provider_image: providerImage,
+					product_name: productName,
+					product_image: productImage,
+					price,
+					currency,
+					completed
+				}
+			})
+		})
+
+		// Act
+		let result = await CreatePurchase({
+			tableObjectUuid,
+			providerName,
+			providerImage,
+			productName,
+			productImage,
+			currency
+		}) as ApiResponse<Purchase>
+
+		// Assert for the response
+		assert.equal(result.status, expectedResult.status)
+		assert.equal(result.data.Id, expectedResult.data.Id)
+		assert.equal(result.data.UserId, expectedResult.data.UserId)
+		assert.equal(result.data.TableObjectId, expectedResult.data.TableObjectId)
+		assert.equal(result.data.ProviderName, expectedResult.data.ProviderName)
+		assert.equal(result.data.ProviderImage, expectedResult.data.ProviderImage)
+		assert.equal(result.data.ProductName, expectedResult.data.ProductName)
+		assert.equal(result.data.ProductImage, expectedResult.data.ProductImage)
+		assert.equal(result.data.Price, expectedResult.data.Price)
+		assert.equal(result.data.Currency, expectedResult.data.Currency)
+		assert.equal(result.data.Completed, expectedResult.data.Completed)
+	})
+})
