3603454814520c41c9502bcbc8e420acaab237bf
David <dav2070@web.de>
2022-05-03 20:02:08 +0200

    Remove CreatePurchase and CompletePurchase functions and tests

--- lib/controllers/PurchasesController.ts
+++ lib/controllers/PurchasesController.ts
@@ -5,60 +5,6 @@ import { ConvertErrorToApiErrorResponse, HandleApiError } from '../utils.js'
 import { Auth } from '../models/Auth.js'
 import { Purchase } from '../models/Purchase.js'
 
-export async function CreatePurchase(params: {
-	accessToken?: string,
-	providerName: string,
-	providerImage: string,
-	productName: string,
-	productImage: string,
-	currency: Currency,
-	tableObjects: string[]
-}): Promise<ApiResponse<Purchase> | ApiErrorResponse> {
-	try {
-		let response = await axios({
-			method: 'post',
-			url: `${Dav.apiBaseUrl}/purchase`,
-			headers: {
-				Authorization: params.accessToken != null ? params.accessToken : Dav.accessToken
-			},
-			data: {
-				provider_name: params.providerName,
-				provider_image: params.providerImage,
-				product_name: params.productName,
-				product_image: params.productImage,
-				currency: params.currency,
-				table_objects: params.tableObjects
-			}
-		})
-
-		return {
-			status: response.status,
-			data: {
-				Id: response.data.id,
-				UserId: response.data.user_id,
-				Uuid: response.data.uuid,
-				PaymentIntentId: response.data.payment_intent_id,
-				ProviderName: response.data.provider_name,
-				ProviderImage: response.data.provider_image,
-				ProductName: response.data.product_name,
-				ProductImage: response.data.product_image,
-				Price: response.data.price,
-				Currency: response.data.currency,
-				Completed: response.data.completed
-			}
-		}
-	} catch (error) {
-		if (params.accessToken != null) {
-			return ConvertErrorToApiErrorResponse(error)
-		}
-
-		let renewSessionError = await HandleApiError(error)
-		if (renewSessionError != null) return renewSessionError
-
-		return await CreatePurchase(params)
-	}
-}
-
 export async function GetPurchase(params: {
 	auth: Auth,
 	uuid: string
@@ -90,47 +36,6 @@ export async function GetPurchase(params: {
 		}
 	} catch (error) {
 		return ConvertErrorToApiErrorResponse(error)
-	}
-}
-
-export async function CompletePurchase(params: {
-	accessToken?: string,
-	uuid: string
-}): Promise<ApiResponse<Purchase> | ApiErrorResponse> {
-	try {
-		let response = await axios({
-			method: 'post',
-			url: `${Dav.apiBaseUrl}/purchase/${params.uuid}/complete`,
-			headers: {
-				Authorization: params.accessToken != null ? params.accessToken : Dav.accessToken
-			}
-		})
-
-		return {
-			status: response.status,
-			data: {
-				Id: response.data.id,
-				UserId: response.data.user_id,
-				Uuid: response.data.uuid,
-				PaymentIntentId: response.data.payment_intent_id,
-				ProviderName: response.data.provider_name,
-				ProviderImage: response.data.provider_image,
-				ProductName: response.data.product_name,
-				ProductImage: response.data.product_image,
-				Price: response.data.price,
-				Currency: response.data.currency,
-				Completed: response.data.completed
-			}
-		}
-	} catch (error) {
-		if (params.accessToken != null) {
-			return ConvertErrorToApiErrorResponse(error)
-		}
-
-		let renewSessionError = await HandleApiError(error)
-		if (renewSessionError != null) return renewSessionError
-
-		return await CompletePurchase(params)
 	}
 }
 
