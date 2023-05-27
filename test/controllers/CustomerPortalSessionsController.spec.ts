4842da382cca66e7b7d596dccd481b948f961d61
David <zurek.dav@gmail.com>
2023-05-27 19:47:07 +0200

    Replace moxios with axios-mock-adapter in CustomerPortalSessionsController tests

--- test/controllers/CustomerPortalSessionsController.spec.ts
+++ test/controllers/CustomerPortalSessionsController.spec.ts
@@ -1,5 +1,6 @@
 import { assert } from "chai"
-import moxios from "moxios"
+import axios from "axios"
+import MockAdapter from "axios-mock-adapter"
 import { Dav } from "../../lib/Dav.js"
 import { ApiResponse, ApiErrorResponse } from "../../index.js"
 import * as ErrorCodes from "../../lib/errorCodes.js"
@@ -8,12 +9,10 @@ import {
 	CreateCustomerPortalSessionResponseData
 } from "../../lib/controllers/CustomerPortalSessionsConroller.js"
 
-beforeEach(() => {
-	moxios.install()
-})
+let mock: MockAdapter = new MockAdapter(axios)
 
-afterEach(() => {
-	moxios.uninstall()
+beforeEach(() => {
+	mock.reset()
 })
 
 describe("CreateCustomerPortalSession function", () => {
@@ -33,20 +32,15 @@ describe("CreateCustomerPortalSession function", () => {
 				}
 			}
 
-		moxios.wait(() => {
-			let request = moxios.requests.mostRecent()
+		mock.onPost(url).reply(config => {
+			assert.equal(config.headers.Authorization, accessToken)
 
-			// Assert for the request
-			assert.equal(request.config.url, url)
-			assert.equal(request.config.method, "post")
-			assert.equal(request.config.headers.Authorization, accessToken)
-
-			request.respondWith({
-				status: expectedResult.status,
-				response: {
+			return [
+				expectedResult.status,
+				{
 					session_url: sessionUrl
 				}
-			})
+			]
 		})
 
 		// Act
@@ -74,17 +68,12 @@ describe("CreateCustomerPortalSession function", () => {
 			]
 		}
 
-		moxios.wait(() => {
-			let request = moxios.requests.mostRecent()
+		mock.onPost(url).reply(config => {
+			assert.equal(config.headers.Authorization, accessToken)
 
-			// Assert for the request
-			assert.equal(request.config.url, url)
-			assert.equal(request.config.method, "post")
-			assert.equal(request.config.headers.Authorization, accessToken)
-
-			request.respondWith({
-				status: expectedResult.status,
-				response: {
+			return [
+				expectedResult.status,
+				{
 					errors: [
 						{
 							code: expectedResult.errors[0].code,
@@ -92,7 +81,7 @@ describe("CreateCustomerPortalSession function", () => {
 						}
 					]
 				}
-			})
+			]
 		})
 
 		// Act
@@ -121,18 +110,13 @@ describe("CreateCustomerPortalSession function", () => {
 				}
 			}
 
-		// First createCustomerPortalSession request
-		moxios.wait(() => {
-			let request = moxios.requests.mostRecent()
+		mock.onPost(url).replyOnce(config => {
+			// First createCustomerPortalSession request
+			assert.equal(config.headers.Authorization, accessToken)
 
-			// Assert for the request
-			assert.equal(request.config.url, url)
-			assert.equal(request.config.method, "post")
-			assert.equal(request.config.headers.Authorization, accessToken)
-
-			request.respondWith({
-				status: 403,
-				response: {
+			return [
+				403,
+				{
 					errors: [
 						{
 							code: ErrorCodes.AccessTokenMustBeRenewed,
@@ -140,41 +124,27 @@ describe("CreateCustomerPortalSession function", () => {
 						}
 					]
 				}
-			})
-		})
+			]
+		}).onPut(`${Dav.apiBaseUrl}/session/renew`).replyOnce(config => {
+			// renewSession request
+			assert.equal(config.headers.Authorization, accessToken)
 
-		// renewSession request
-		moxios.wait(() => {
-			let request = moxios.requests.mostRecent()
-
-			// Assert for the request
-			assert.equal(request.config.url, `${Dav.apiBaseUrl}/session/renew`)
-			assert.equal(request.config.method, "put")
-			assert.equal(request.config.headers.Authorization, accessToken)
-
-			request.respondWith({
-				status: 200,
-				response: {
+			return [
+				200,
+				{
 					access_token: newAccessToken
 				}
-			})
-		})
+			]
+		}).onPost(url).replyOnce(config => {
+			// Second createCustomerPortalSession request
+			assert.equal(config.headers.Authorization, newAccessToken)
 
-		// Second createCustomerPortalSession request
-		moxios.wait(() => {
-			let request = moxios.requests.mostRecent()
-
-			// Assert for the request
-			assert.equal(request.config.url, url)
-			assert.equal(request.config.method, "post")
-			assert.equal(request.config.headers.Authorization, newAccessToken)
-
-			request.respondWith({
-				status: expectedResult.status,
-				response: {
+			return [
+				expectedResult.status,
+				{
 					session_url: sessionUrl
 				}
-			})
+			]
 		})
 
 		// Act
