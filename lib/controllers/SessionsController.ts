afb1056df2c3472e9b48877e9bc0f55254e446a0
David <zurek.dav@gmail.com>
2025-01-08 22:39:26 +0100

    Remove SessionsController.CreateSession & implement new createSession function using the new API

--- lib/controllers/SessionsController.ts
+++ lib/controllers/SessionsController.ts
@@ -14,41 +14,58 @@ export interface SessionResponseData {
 	websiteAccessToken?: string
 }
 
-export async function CreateSession(params: {
-	auth: Auth
-	email: string
-	password: string
-	appId: number
-	apiKey: string
-	deviceName?: string
-	deviceOs?: string
-}): Promise<ApiResponse<SessionResponseData> | ApiErrorResponse> {
+export async function createSession(
+	queryData: string,
+	variables: {
+		auth: Auth
+		email: string
+		password: string
+		appId: number
+		apiKey: string
+		deviceName: string
+		deviceOs: string
+	}
+): Promise<SessionResponseData | ErrorCode[]> {
 	try {
-		let response = await axios({
-			method: "post",
-			url: `${Dav.apiBaseUrl}/session`,
-			headers: {
-				Authorization: params.auth.token
+		let response = await request<{ createSession: SessionResponseData }>(
+			Dav.newApiBaseUrl,
+			gql`
+				mutation CreateSession(
+					$email: String!
+					$password: String!
+					$appId: Int!
+					$apiKey: String!
+					$deviceName: String
+					$deviceOs: String
+				) {
+					createSession(
+						email: $email
+						password: $password
+						appId: $appId
+						apiKey: $apiKey
+						deviceName: $deviceName
+						deviceOs: $deviceOs
+					) {
+						${queryData}
+					}
+				}
+			`,
+			{
+				email: variables.email,
+				password: variables.password,
+				appId: variables.appId,
+				apiKey: variables.apiKey,
+				deviceName: variables.deviceName,
+				deviceOs: variables.deviceOs
 			},
-			data: PrepareRequestParams({
-				email: params.email,
-				password: params.password,
-				app_id: params.appId,
-				api_key: params.apiKey,
-				device_name: params.deviceName,
-				device_os: params.deviceOs
-			})
-		})
-
-		return {
-			status: response.status,
-			data: {
-				accessToken: response.data.access_token,
-				websiteAccessToken: response.data.website_access_token
+			{
+				Authorization: variables.auth.token
 			}
-		}
+		)
+
+		return response.createSession
 	} catch (error) {
-		return ConvertErrorToApiErrorResponse(error)
+		return getErrorCodesOfGraphQLError(error as ClientError)
 	}
 }
 
@@ -109,11 +126,7 @@ export async function renewSession(
 			}
 		)
 
-		if (response.renewSession == null) {
-			return null
-		} else {
-			response.renewSession
-		}
+		return response.renewSession
 	} catch (error) {
 		return getErrorCodesOfGraphQLError(error as ClientError)
 	}
