f0a6bbbb6a5bd38c2128404b8a5ef6d4d2cf32b7
David <zurek.dav@gmail.com>
2025-01-22 00:03:22 +0100

    Add CheckoutSessionsController.createPaymentCheckoutSession function

--- lib/controllers/CheckoutSessionsController.ts
+++ lib/controllers/CheckoutSessionsController.ts
@@ -1,6 +1,12 @@
 import { request, gql, ClientError } from "graphql-request"
 import { Dav } from "../Dav.js"
-import { ErrorCode, CheckoutSessionResource, Plan } from "../types.js"
+import {
+	ErrorCode,
+	CheckoutSessionResource,
+	Plan,
+	TableObjectPriceType,
+	Currency
+} from "../types.js"
 import { getErrorCodesOfGraphQLError, handleGraphQLErrors } from "../utils.js"
 
 export async function createSubscriptionCheckoutSession(
@@ -54,5 +60,86 @@ export async function createSubscriptionCheckoutSession(
 		if (renewSessionError != null) return renewSessionError as ErrorCode[]
 
 		return await createSubscriptionCheckoutSession(queryData, variables)
+	}
+}
+
+export async function createPaymentCheckoutSession(
+	queryData: string,
+	variables: {
+		accessToken?: string
+		tableObjectUuid: string
+		type: TableObjectPriceType
+		price?: number
+		currency?: Currency
+		productName: string
+		productImage: string
+		shippingRate?: {
+			name: string
+			price: number
+		}
+		successUrl: string
+		cancelUrl: string
+	}
+): Promise<CheckoutSessionResource | ErrorCode[]> {
+	try {
+		let response = await request<{
+			createPaymentCheckoutSession: CheckoutSessionResource
+		}>(
+			Dav.newApiBaseUrl,
+			gql`
+				mutation CreatePaymentCheckoutSession(
+					$tableObjectUuid: String!
+					$type: TableObjectPriceType!
+					$price: Int
+					$currency: Currency
+					$productName: String!
+					$productImage: String!
+					$shippingRate: ShippingRate
+					$successUrl: String!
+					$cancelUrl: String!
+				) {
+					createSubscriptionCheckoutSession(
+						tableObjectUuid: $tableObjectUuid
+						type: $type
+						price: $price
+						currency: $currency
+						productName: $productName
+						productImage: $productImage
+						shippingRate: $shippingRate
+						successUrl: $successUrl
+						cancelUrl: $cancelUrl
+					) {
+						${queryData}			
+					}
+				}
+			`,
+			{
+				tableObjectUuid: variables.tableObjectUuid,
+				type: variables.type,
+				price: variables.price,
+				currency: variables.currency,
+				productName: variables.productName,
+				productImage: variables.productImage,
+				shippingRate: variables.shippingRate,
+				successUrl: variables.successUrl,
+				cancelUrl: variables.cancelUrl
+			},
+			{
+				Authorization: variables.accessToken ?? Dav.accessToken
+			}
+		)
+
+		return response.createPaymentCheckoutSession
+	} catch (error) {
+		const errorCodes = getErrorCodesOfGraphQLError(error as ClientError)
+
+		if (variables.accessToken != null) {
+			return errorCodes
+		}
+
+		let renewSessionError = await handleGraphQLErrors(errorCodes)
+		if (renewSessionError != null) return renewSessionError as ErrorCode[]
+
+		return await createPaymentCheckoutSession(queryData, variables)
 	}
 }
